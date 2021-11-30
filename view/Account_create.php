5a4c3f760a47d2610d59d4bf37a9d1753e0b910a
HOTosiruko <94329741+HOTosiruko@users.noreply.github.com>
2021-11-30 10:00:27 +0900

    アカウント作成画面:APIお試し中

--- view/Account_create.php
+++ view/Account_create.php
@@ -16,12 +16,35 @@
         <tr>
             <td>
                 <div align="center">
-                    <table border="0">
-                        <form action="list.html" method="get">
-                        <tr>
-                                <th>
+                    <form action="list.html" method="get">
+                        <table>
+
+                            <tr>
+                                <td>
+                                    姓
+                                    <input type="text" name="surname" size="24" required>
+                                </td>
+                                <td>
+                                    名
+                                    <input type="text" name="name" size="24" required>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    姓フリガナ
+                                    <input type="text" name="surname_furigana" size="24" requiredHTML テキストボックス>
+                                </td>
+                                <td>
+                                    名フリガナ
+                                    <input type="text" name="name_furigana" size="24" required>
+                                </td>
+                            </tr>
+                        </table>
+                        <table>
+                            <tr>
+                                <td>
                                     性別
-                                </th>
+                                </td>
                                 <td align="center">
                                     <label>
                                         <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" required>
@@ -39,81 +62,166 @@
                                 </td>
                             </tr>
                             <tr>
-                                <th>
-                                    お名前
-                                </th>
+                                <td>西暦</td>
                                 <td>
-                                    <input type="text" name="name" size="24" required>
-                                </td>
+                                    <select name="year">
+                                            <option value="">--</option>
+                                            <?php foreach (range(1920, 2022) as $year) : ?>
+                                                <option value="<?= $year ?>"><?= $year ?></option>
+                                            <?php endforeach; ?>
+                                        </select>年
+                                        <select name="month">
+                                           
+                                        </select>月
+                                        <select name="day">
+                                            <option value="">-</option>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                            <option value="5">5</option>
+                                            <option value="6">6</option>
+                                            <option value="7">7</option>
+                                            <option value="8">8</option>
+                                            <option value="9">9</option>
+                                            <option value="10">10</option>
+                                            <option value="11">11</option>
+                                            <option value="12">12</option>
+                                            <option value="13">13</option>
+                                            <option value="14">14</option>
+                                            <option value="15">15</option>
+                                            <option value="16">16</option>
+                                            <option value="17">17</option>
+                                            <option value="18">18</option>
+                                            <option value="19">19</option>
+                                            <option value="20">20</option>
+                                            <option value="21">21</option>
+                                            <option value="22">22</option>
+                                            <option value="23">23</option>
+                                            <option value="24">24</option>
+                                            <option value="25">25</option>
+                                            <option value="26">26</option>
+                                            <option value="27">27</option>
+                                            <option value="28">28</option>
+                                            <option value="29">29</option>
+                                            <option value="30">30</option>
+                                            <option value="31">31</option>
+                                        </select>日
                             </tr>
                             <tr>
-                                <th>
-                                    フリガナ
-                                </th>
                                 <td>
-                                    <input type="text" name="furigana" size="24" required>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>
-                                    生年月日
-                                </th>
-                                <td>
-                                    <input type="text" name="birthday" size="24" required>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>
                                     郵便番号
-                                </th>
+                                </td>
                                 <td>
                                     <input type="text" name="postal_code" placeholder="ハイフンなしの、半角で入力してください" required size="7">
                                 </td>
                             </tr>
                             <tr>
-                                <th>
-                                    住所
-                                </th>
+                                <td>都道府県</td>
+                                <td>
+                                    <select name="user_prefectures">
+                                        <option value="" selected>選択してください</option>
+                                        <option value="北海道">北海道</option>
+                                        <option value="青森県">青森県</option>
+                                        <option value="岩手県">岩手県</option>
+                                        <option value="宮城県">宮城県</option>
+                                        <option value="秋田県">秋田県</option>
+                                        <option value="山形県">山形県</option>
+                                        <option value="福島県">福島県</option>
+                                        <option value="茨城県">茨城県</option>
+                                        <option value="栃木県">栃木県</option>
+                                        <option value="群馬県">群馬県</option>
+                                        <option value="埼玉県">埼玉県</option>
+                                        <option value="千葉県">千葉県</option>
+                                        <option value="東京都">東京都</option>
+                                        <option value="神奈川県">神奈川県</option>
+                                        <option value="新潟県">新潟県</option>
+                                        <option value="富山県">富山県</option>
+                                        <option value="石川県">石川県</option>
+                                        <option value="福井県">福井県</option>
+                                        <option value="山梨県">山梨県</option>
+                                        <option value="長野県">長野県</option>
+                                        <option value="岐阜県">岐阜県</option>
+                                        <option value="静岡県">静岡県</option>
+                                        <option value="愛知県">愛知県</option>
+                                        <option value="三重県">三重県</option>
+                                        <option value="滋賀県">滋賀県</option>
+                                        <option value="京都府">京都府</option>
+                                        <option value="大阪府">大阪府</option>
+                                        <option value="兵庫県">兵庫県</option>
+                                        <option value="奈良県">奈良県</option>
+                                        <option value="和歌山県">和歌山県</option>
+                                        <option value="鳥取県">鳥取県</option>
+                                        <option value="島根県">島根県</option>
+                                        <option value="岡山県">岡山県</option>
+                                        <option value="広島県">広島県</option>
+                                        <option value="山口県">山口県</option>
+                                        <option value="徳島県">徳島県</option>
+                                        <option value="香川県">香川県</option>
+                                        <option value="愛媛県">愛媛県</option>
+                                        <option value="高知県">高知県</option>
+                                        <option value="福岡県">福岡県</option>
+                                        <option value="佐賀県">佐賀県</option>
+                                        <option value="長崎県">長崎県</option>
+                                        <option value="熊本県">熊本県</option>
+                                        <option value="大分県">大分県</option>
+                                        <option value="宮崎県">宮崎県</option>
+                                        <option value="鹿児島県">鹿児島県</option>
+                                        <option value="沖縄県">沖縄県</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    市町村名
+                                </td>
+                                <td>
+                                    <input type="text" name="address" 　placeholder="数字は半角で入力してください" required size="56">
+                                </td>
+                            <tr>
+                                <td>
+                                    部屋番号
+                                </td>
                                 <td>
                                     <input type="text" name="address" 　placeholder="数字は半角で入力してください" required size="56">
                                 </td>
                             </tr>
                             <tr>
-                                <th>
+                                <td>
                                     電話番号
-                                </th>
+                                </td>
                                 <td>
                                     <input type="tel" name="tel" placeholder="ハイフンなしの、半角で入力してください" required size="24">
                                 </td>
                             </tr>
                             <tr>
-                                <th>
+                                <td>
                                     メールアドレス
-                                </th>
+                                </td>
                                 <td>
                                     <input type="email" name="usermail" placeholder="半角で入力してください" required size="56">
                                 </td>
                             </tr>
                             <tr>
-                                <th>
+                                <td>
                                     ユーザID
-                                </th>
+                                </td>
                                 <td>
                                     <input type="text" name="user_id" size="24" required>
                                 </td>
                             </tr>
                             <tr>
-                                <th>
+                                <td>
                                     パスワード
-                                </th>
+                                </td>
                                 <td>
                                     <input type="password" name="password" size="24" required>
                                 </td>
                             </tr>
                             <tr>
-                                <th>
+                                <td>
                                     確認用パスワード
-                                </th>
+                                </td>
                                 <td>
                                     <input type="password" name="re_password" size="24" required>
                                 </td>
@@ -125,15 +233,15 @@
                                     </div>
                                 </td>
                             </tr>
-                        </form>
-                    </table>
+                        </table>
+                    </form>
                 </div>
             </td>
         </tr>
-    </table>
-    <!------------------------------------------- footer ------------------------------------------->
-    <?php include 'frame/footer.php'; ?>
-    <!------------------------------------------- footer ------------------------------------------->
+        </form>
+        <!------------------------------------------- footer ------------------------------------------->
+        <?php include 'frame/footer.php'; ?>
+        <!------------------------------------------- footer ------------------------------------------->
 </body>
 
 </html>
