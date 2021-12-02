8795711b52a86e0ff452b314dfbb82a089a25f63
mitunari321kg <75006637+mitunari321kg@users.noreply.github.com>
2021-12-02 15:52:30 +0900

    管理者画面：ヘッダー変更

--- admin/view/frame/Admin_Header.php
+++ admin/view/frame/Admin_Header.php
@@ -11,6 +11,13 @@
             </td>
         </tr>
         <tr>
+            <td align="right">
+                <button type="submit" class="btn btn-dark" onclick="location.href='Admin_Login.php'">
+                    <font color="white"><i class="bi bi-person-fill text-nowrap">ログアウト</i></font>
+                </button>
+            </td>
+        </tr>
+        <tr>
             <td width="100%">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <div class="collapse navbar-collapse justify-content-center container-fluid " id="navbar2">
