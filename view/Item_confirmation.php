dbe827f8ac6cb7dd2155bbe45379ff039ab85f5d
TReo1207 <r6sryu123ki@gmail.com>
2021-12-07 10:21:07 +0900

    ファイル名変更(スペースを_に変更)、

---
+++ view/Item_confirmation.php
@@ -0,0 +1,91 @@
+<!-------入力項目確認画面
+作成者　梅原-------->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'frame/basic_style_info.php'; ?>
+    <link href="css/Item confirmation.css" rel="stylesheet" />
+    <title>入力項目確認</title>
+</head>
+
+<body>
+    <!------------------------------------------- header ------------------------------------------->
+    <?php include 'frame/header.php'; ?>
+    <!------------------------------------------- header ------------------------------------------->
+    <table width="100%">
+        <tr>
+            <td>
+                <div align="center">
+                    <form action="list.html" method="get">
+                        <table>
+                            <tr>
+                                <td>
+                                    姓
+                                    <input type="text" name="surname" size="24" required>
+                                </td>
+                                <td>
+                                    名
+                                    <input type="text" name="name" size="24" required>
+                                </td>
+                            </tr>
+                        </table>
+                        <table>
+                            <tr>
+                                <td>
+                                    郵便番号
+                                </td>
+                                <td>
+                                    <input type="text" name="postal_code" required size="7">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    お届け先住所
+                                </td>
+                                <td>
+                                    <input type="text" name="surname" size="24" required>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    電話番号
+                                </td>
+                                <td>
+                                    <input type="tel" name="tel" required size="24">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    メールアドレス
+                                </td>
+                                <td>
+                                    <input type="email" name="usermail" required size="56">
+                                </td>
+                            </tr>
+                        </table>
+                        <table>
+                            <tr>
+                                <td>
+                                    <button type="submit" class="nav-item btn btn-dark text-nowrap">
+                                        <font color="white">確認画面へ</font>
+                                    </button>
+                                    
+                                    <button type="submit" class="nav-item btn btn-dark text-nowrap">
+                                        <font color="white">別住所にお届けの方</font>
+                                    </button>
+                                </td>
+                            </tr>
+                        </table>
+                    </form>
+                </div>
+            </td>
+        </tr>
+    </table>
+    <!------------------------------------------- footer ------------------------------------------->
+    <?php include 'frame/footer.php'; ?>
+    <!------------------------------------------- footer ------------------------------------------->
+</body>
+
+</html>
