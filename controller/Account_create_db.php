fad13467432d418c9c356216a1316bd834d2f8d2
mitunari321kg <75006637+mitunari321kg@users.noreply.github.com>
2021-12-20 10:46:17 +0900

    ログイン処理追加、cssのフォント更新

--- controller/Account_create_db.php
+++ controller/Account_create_db.php
@@ -65,7 +65,7 @@ class Account_create_db extends Controll
             if ($this->db->exec_sql_insert('user_table', $params, $styles)) {
                 //登録完了
                 $_SESSION['result_msg'] = "<br><font color=GREEN>登録が完了しました。</font></br>";
-                header('Location: ../view/contact_result.php');
+                header('Location: ../view/Login.php');
             } else {
                 $_SESSION['result_msg'] = "<br><font color=RED>※エラーが発生しました。</font></br>";
                 header('Location: ../view/Account_create.php');
