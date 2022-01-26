857a7b5d80be03f6837636eafc8deb1e5b363c73
HOTosiruko <94329741+HOTosiruko@users.noreply.github.com>
2022-01-26 10:22:21 +0900

    カートのやつ

--- controller/Verification_DB.php
+++ controller/Verification_DB.php
@@ -18,55 +18,9 @@ class Verification_DB extends Control
      * 
      */
     public function insert_employee()
-<<<<<<< HEAD
-{
-            $params1 = array(
 
-                $_SESSION['shipping_info']['last_name'] + $_SESSION['shipping_info']['first_name'] => $_POST['name'],
-                $_SESSION['shipping_info']['last_furigana'] + $_SESSION['shipping_info']['first_furigana'] => $_POST['name_furigana'],
-
-                $_SESSION['shipping_info']['postal_code'] => $_POST['postal_code'],
-                $_SESSION['shipping_info']['prefactures'] => $_POST['prefactures'],
-
-                $_SESSION['shipping_info']['address01'] => $_POST['address01'],
-                $_SESSION['shipping_info']['address02'] => $_POST['address02'],
-                $_SESSION['shipping_info']['address03'] =>$_POST['address03'],
-                
-                $_SESSION['shipping_info']['tel'] => $_POST['tel'],
-                $_SESSION['shipping_info']['mail'] => $_POST['mail']
-
-            );
-            
-            $styles1= array(
-
-                PDO::PARAM_STR,
-                PDO::PARAM_STR,
-
-                PDO::PARAM_STR,
-                PDO::PARAM_STR,
-
-                PDO::PARAM_STR,
-                PDO::PARAM_STR,
-                PDO::PARAM_STR,
-
-                PDO::PARAM_STR,
-                PDO::PARAM_STR,
-                
-            );
-
-            $params02 = array(
-                $_SESSION['cart']['quantity'] => $_POST[['quantity']
-            );
-
-            if ($this->db->exec_sql_insert('delivery', $params1, $styles1)) {
-
-                //登録完了
-
-                $_SESSION['result_msg'] = "<br><font color=GREEN>登録しました</font></br>";
-                header('Location: ../view/Verification.php');
-=======
     {
->>>>>>> origin/tanihara
+
 
         $date = date('Y-m-d H:i:s');
         //print_r($_POST['products']);
